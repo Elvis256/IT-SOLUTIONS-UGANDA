e102347fe0f90cc233a666fecbb5ebf32fa27a8a
Elvis256 <elvisdanntuyo>
2025-11-13 13:05:48 +0300

    Fix logger for serverless and make MongoDB optional

--- config/database.js
+++ config/database.js
@@ -3,6 +3,11 @@ const logger = require('./logger');
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGODB_URI) {
+      logger.warn('MONGODB_URI not configured - database features will be unavailable');
+      return;
+    }
+    
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
